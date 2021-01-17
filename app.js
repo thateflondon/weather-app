1905b1b1cdbda8f48b40c5a92b647acff3c3203d
Jerry Frederic <jerry.fpl@gmail.com>
2021-01-18 00:59:33 +0100

    affichage temperature

---
+++ app.js
@@ -0,0 +1,24 @@
+//URL du site qui renvoie le document JSON
+
+let ville = "Douala";
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=9475ab79b57072017a830d1a4ab5da82&units=metric';
+
+//Requete AJAX
+
+let requete = new XMLHttpRequest();
+requete.open('GET', url);
+requete.responseType = 'json';
+requete.send();
+
+requete.onload = function () {
+    if (requete.readyState === XMLHttpRequest.DONE) {
+        if (requete.status === 200) {
+            let reponse = requete.response;
+            //console.log(reponse);
+            let temperature = reponse.main.temp;
+            document.querySelector('#temperature_label').textContent = temperature;
+        } else {
+            alert('Un problème est survenu, merci de revenir plus tard.');
+        }
+    }
+}

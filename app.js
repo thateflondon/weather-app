98879e3dfe909b0eb422270b6a10fd6a36d7d7ce
Jerry Frederic <jerry.fpl@gmail.com>
2021-01-18 03:37:14 +0100

    affichage temperature par defaut

--- app.js
+++ app.js
@@ -1,27 +1,46 @@
-//URL du site qui renvoie le document JSON
-
+// Ville par défaut
 let ville = "Douala";
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=9475ab79b57072017a830d1a4ab5da82&units=metric';
 
-//Requete AJAX
+//Création de l'évènement
+let changerDeVille = document.querySelector('#changer');
+changerDeVille.addEventListener('click', () => {
+    villeChoisie = prompt('Saisissez le nom d\'une ville');
+    //permet d'afficher la température de la ville par défaut
+    recevoirTemperature(ville);
+});
 
-let requete = new XMLHttpRequest();
-requete.open('GET', url);
-requete.responseType = 'json';
-requete.send();
 
-requete.onload = function () {
-    if (requete.readyState === XMLHttpRequest.DONE) {
-        if (requete.status === 200) {
-            let reponse = requete.response;
-            //console.log(reponse);
-            let temperature = reponse.main.temp;
-            document.querySelector('#temperature_label').textContent = temperature;
-            //let reponseVille = requete.response;
-            let ville = reponse.name;
-            document.querySelector('#ville').textContent = ville;
-        } else {
-            alert('Un problème est survenu, merci de revenir plus tard.');
+//Fonction 
+function recevoirTemperature(ville) {
+
+    //URL du site qui renvoie le document JSON
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=9475ab79b57072017a830d1a4ab5da82&units=metric';
+
+    //Requete AJAX
+    let requete = new XMLHttpRequest();
+    requete.open('GET', url);
+    requete.responseType = 'json';
+    requete.send();
+
+    requete.onload = function () {
+        if (requete.readyState === XMLHttpRequest.DONE) {
+            if (requete.status === 200) {
+                let reponse = requete.response;
+                //console.log(reponse);
+                let temperature = reponse.main.temp;
+                document.querySelector('#temperature_label').textContent = temperature;
+                //let reponseVille = requete.response;
+                let ville = reponse.name;
+                document.querySelector('#ville').textContent = ville;
+            } else {
+                alert('Un problème est survenu, merci de revenir plus tard.');
+            }
         }
     }
+
 }
+
+
+
+
+
